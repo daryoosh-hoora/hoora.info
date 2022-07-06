4f15583865bb1ee0e5b8b5217f56181f74a3c157
Daryoosh Hoora <daryoosh@hoora.info>
2022-07-06 22:29:35 +0430

    Add project files.

---
+++ hoora.info_MVC/wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+window.onload = function () {
+    const darktoggle = document.getElementById("darkmode")
+
+    darktoggle.addEventListener("change", function (e) {
+        document.body.classList.toggle("hoora-darkmode", e.target.checked)
+    })
+}
